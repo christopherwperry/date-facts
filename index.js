4fb5d7a1524d6a07427a1d1f0e0438ea9c33ab8d
christopherwperry <christopherwperry@gmail.com>
2017-08-14 15:21:18 -0400

    condense method for obtaining seconds into day

--- index.js
+++ index.js
@@ -4,15 +4,12 @@ const moment = require('moment');
 
 let now = moment().format('LLLL');
 let day = moment().format('DDD');
-let seconds = moment().format('');
 let dls = moment().isDST();
 let year = 2017
 let status;
 let leap_status;
 let leap = moment(year).isLeapYear();
-let mmt = moment();
-let mmtMidnight = mmt.clone().startOf('day');
-let diffSec = mmt.diff(mmtMidnight, 'seconds');
+let seconds = moment().diff(moment().hour(0).minute(0).second(0), 'seconds');
 
 if (dls) {
   status = "is";
@@ -28,6 +25,6 @@ if (leap) {
 
 console.log("Today is: " + (chalk.blueBright(now)) + ".");
 console.log("It is the " + (chalk.magentaBright(day)) + (chalk.magentaBright("th")) + " day of the year.");
-console.log("It is " + chalk.cyanBright(diffSec) + " seconds into the day.");
+console.log("It is " + chalk.cyanBright(seconds) + " seconds into the day.");
 console.log("It " + (chalk.green(status)) + " during Daylight Savings Time.");
 console.log("It " + (chalk.red(leap_status))+ " a leap year.");
